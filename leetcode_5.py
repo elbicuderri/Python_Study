166510a91e0849749ac65863a032a9eb1fed4261
seung.hwan <46323118+elbicuderri@users.noreply.github.com>
2021-01-01 21:52:20 +0900

    Add files via upload

---
+++ leetcode_5.py
@@ -0,0 +1,53 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        L = len(s)
+        if L == 1:
+            return s
+        max_p = s[0]
+        def isPalindrome(word):
+            if word == word[::-1]:
+                return True
+            else: return False
+        
+        max_len = 1
+        for i in range(L):
+            if max_len >= L-i:
+                break
+            for j in range(i, L):
+                    target = s[i:j+1]
+                    if isPalindrome(target) == True:
+                        if len(target) > len(max_p):
+                            max_p = target
+                            max_len = len(target)
+                    
+        return max_p
+        
+case1 = "babad"
+
+case2 = "cbbd"
+
+case3 = "a"
+
+case4 = "ac"
+
+case5 = "bb"
+
+case6 = "dqmvxouqesajlmksdawfenyaqtnnfhmqbdcniynwhuywucbjzqxhofdzvposbegkvqqrdehxzgikgtibimupumaetjknrjjuygxvncvjlahdbibatmlobctclgbmihiphshfpymgtmpeneldeygmzlpkwzouvwvqkunihmzzzrqodtepgtnljribmqneumbzusgppodmqdvxjhqwqcztcuoqlqenvuuvgxljcnwqfnvilgqrkibuehactsxphxkiwnubszjflvvuhyfwmkgkmlhmvhygncrtcttioxndbszxsyettklotadmudcybhamlcjhjpsmfvvchduxjngoajclmkxiugdtryzinivuuwlkejcgrscldgmwujfygqrximksecmfzathdytauogffxcmfjsczaxnfzvqmylujfevjwuwwaqwtcllrilyncmkjdztleictdebpkzcdilgdmzmvcllnmuwpqxqjmyoageisiaeknbwzxxezfbfejdfausfproowsyyberhiznfmrtzqtgjkyhutieyqgrzlcfvfvxawbcdaawbeqmzjrnbidnzuxfwnfiqspjtrszetubnjbznnjfjxfwtzhzejahravwmkakqsmuynklmeffangwicghckrcjwtusfpdyxxqqmfcxeurnsrmqyameuvouqspahkvouhsjqvimznbkvmtqqzpqzyqivsmznnyoauezmrgvproomvqiuzjolejptuwbdzwalfcmweqqmvdhejguwlmvkaydjrjkijtrkdezbipxoccicygmmibflxdeoxvudzeobyyrutbcydusjhmlwnfncahxgswxiupgxgvktwkdxumqp"
+
+case7 = "cstgvkbrxacmpxdxxktktvpdzcuxmnhvuxdgsmskgeeawzeikhtmhdvnccbrgifpzmiuytfmeyfoxsntrdtxeuxcqsndexbgfxnthqwveujqzemloooyddparbjcuiwpopjwvvmwblsamkhjhlnoxinkpsempexmipifsfwzxbetgvfnkngzxcpizwctpdlpngjpyovmjllxfiwktghkxvyelwjwdztujmunijfsfdvmhgqhlpouewgyznphlmccjmqaqncwbeqheohibafqfunfywmrvqvjygjwqoclijwkcfiuaiymeagdbwyejnvtosxylptbtyoahfzfmwzrkhzdamknleroffmsqcaryibamgdpcumlhrblypddzhaxfeztokgogzgvpfvlmetiwsamhdidmvxavleryfyakendwrbslcavlqkerrnvpuzhdgwzuyorxzbkzhxhpbvkflgxouvaavxrdzsjlgrmogzvlhhdidldsxqhrqlryaanffhxnutcycnczuedtrwcnfiqrtoafvdfnfhxhyjivzalozrbrajboecfyalisxxanduzraqdrbzsbkobaieqpzcawrunxucypqyjnmrlrlivrrwwhdpekeelsphhunzbhkkejvqfopjsuholutgmtnleqdrntbqgrobnuhqpdkbjtikijkdiwqvnxgajaaqgswrdamzv"
+
+print(Solution().longestPalindrome(case1))
+
+print(Solution().longestPalindrome(case2))
+
+print(Solution().longestPalindrome(case3))
+
+print(Solution().longestPalindrome(case4))
+
+print(Solution().longestPalindrome(case5))
+
+print(Solution().longestPalindrome(case6))
+
+print(Solution().longestPalindrome(case7))
+
+
